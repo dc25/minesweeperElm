5c56de63224e3bf89e03ae02a3bc959c0714d557
Dave Compton <davecompton7@gmail.com>
2016-12-14 05:15:30 +0000

    showText working

--- Main.elm
+++ Main.elm
@@ -1,10 +1,10 @@
-import List exposing (map2, length)
+import List exposing (map2, length, range)
 import List.Extra exposing (andThen)
 import Dict exposing (Dict, fromList, values, insert, get)
 import Maybe exposing (withDefault)
 import Html exposing (Html, beginnerProgram)
-import Svg exposing (Svg, rect, svg, g)
-import Svg.Attributes exposing (transform, version, x, y, width, height, style)
+import Svg exposing (Svg, rect, svg, g, text_)
+import Svg.Attributes exposing (transform, version, x, y, width, height, style, textAnchor, fill, fontSize)
 import Svg.Events exposing (onClick, on)
 import Json.Decode as Json
 import Html.Events exposing (onWithOptions)
@@ -34,11 +34,11 @@ cellSize = 25
 
 
 onRightClick message =
-  on
-    "oncontextmenu"
-    -- { stopPropagation = True
-    -- , preventDefault = True
-    -- }
+  onWithOptions
+    "contextmenu"
+    { stopPropagation = True
+    , preventDefault = True
+    }
     (Json.succeed message)
 
 generateCell : Generator Cell
@@ -46,9 +46,8 @@ generateCell = map (\t -> Cell (t < 0.201) False False 0) (float 0.0 1.0)
 
 generateBoard : Generator Board
 generateBoard = 
-    let indices = andThen (\r -> 
-                       andThen (\c -> [(r, c)]) 
-                           (List.range 0 (w-1)) ) (List.range 0 (h-1)) 
+    let indices = andThen (\r -> andThen (\c -> [(r, c)]) 
+                              (range 0 (w-1)) ) (range 0 (h-1)) 
     in  map (\cs -> fromList (map2 (,) indices cs))
             (list (length indices) generateCell)
 
@@ -67,17 +66,39 @@ showSquare (row,col) cell =
                , height (toString 0.9)
                , style ("fill:" ++ getColor cell)
                , onClick (LeftPick (row, col))
-               , onRightClick (LeftPick (row, col))
+               , onRightClick (RightPick (row, col))
                ] 
                []
+
+showText : Pos -> Int -> Svg Msg
+showText pos count = 
+    text_ [ x "0.5"
+          , y "0.87" 
+          , fontSize "1.0"
+          -- , dominantBaseline "middle"
+          , fill "blue"
+          , textAnchor "middle"
+          , onClick (LeftPick pos)
+          , onRightClick (RightPick pos)
+          ] 
+          [ Svg.text (toString count)
+          ]
+
+showCellDetail : Pos -> Cell -> Svg Msg
+showCellDetail pos {mined, exposed, flagged, mineCount} = 
+    case (  mined, exposed, flagged, 0 == mineCount) of
+         (      _,       _,    True,     _) -> showText pos mineCount
+         (   True,    True,       _,     _) -> showText pos mineCount
+         (      _,    True,       _, False) -> showText pos mineCount
+         (      _,       _,       _,     _) -> showText pos mineCount
 
 showCell : Pos -> Cell -> Svg Msg
 showCell pos cell = 
     let (row,col) = pos 
     in g [ transform ("scale (" ++ toString cellSize ++ ", " ++ toString cellSize ++ ") " ++ "translate (" ++ toString col ++ ", " ++ toString row ++ ") " )
          ]
-         [ 
-           showSquare pos cell
+         [ showSquare pos cell
+         , showCellDetail pos cell
          ]
 
 view : (Board, Cmd Msg) -> Html Msg
@@ -95,8 +116,9 @@ update msg (model,_) =
         (LeftPick pos) ->
             let c = withDefault (Cell False False False 0) (get pos model)
             in (insert pos ({c|exposed = True}) model, Cmd.none)
-        _ -> 
-            (model, Cmd.none)
+        (RightPick pos) ->
+            let c = withDefault (Cell False False False 0) (get pos model)
+            in (insert pos ({c|flagged = not (c.flagged)}) model, Cmd.none)
 
 main =
   beginnerProgram { 
